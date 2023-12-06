0aaf19270d621f9d1bf6988eab8ecdcd9e31e604
Maryana Sampaio <maryanacem.star2@gmail.com>
2023-12-06 19:41:18 -0300

    envio dos testes de todos os exercicios

--- test/exercicios_de_algoritmos_test.dart
+++ test/exercicios_de_algoritmos_test.dart
@@ -1,8 +1,39 @@
-import 'package:exercicios_de_algoritmos/exercicios_de_algoritmos.dart';
-import 'package:test/test.dart';
+import 'package:exercicios_de_algoritmos/lista1/questao_1.dart';
+import 'package:exercicios_de_algoritmos/lista1/questao_2.dart';
+import 'package:exercicios_de_algoritmos/lista1/questao_3.dart';
+import 'package:exercicios_de_algoritmos/lista1/questao_4.dart';
+import 'package:exercicios_de_algoritmos/lista1/questao_5.dart';
+import 'package:exercicios_de_algoritmos/lista2/questao_1.dart';
+import 'package:exercicios_de_algoritmos/lista2/questao_2.dart';
+import 'package:exercicios_de_algoritmos/lista2/questao_3.dart';
+import 'package:exercicios_de_algoritmos/lista2/questao_4.dart';
+import 'package:exercicios_de_algoritmos/lista2/questao_5.dart';
+import 'package:exercicios_de_algoritmos/lista3/questao_1.dart';
+import 'package:exercicios_de_algoritmos/lista3/questao_2.dart';
+import 'package:exercicios_de_algoritmos/lista3/questao_3.dart';
+import 'package:exercicios_de_algoritmos/lista3/questao_4.dart';
+import 'package:exercicios_de_algoritmos/lista3/questao_5.dart';
+import 'package:exercicios_de_algoritmos/lista3/questao_6.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
+  void main() {
+    trocarValores(20, 35);
+    operacoesAritmeticas(3.2, 5.2);
+    tipoDeVariavel("teste");
+
+    int r = inverterNumeros(379);
+    print(r);
+    caixaEletronico(489);
+    retornarMaiorNumero(3, 6);
+    parOuImpar(2);
+    classificarIdade(12);
+    calculadoraDeBonus(1000, 8);
+    tipoDeTriangulo(1, 2, 3);
+    somaDosQuadrados(5);
+    contagemRegressiva(10);
+    fatorialDeUmNumero(3);
+    print(verificarSeEhPrimo(2));
+    print(verificaPrimo(2));
+    verificaPalindromo('ama');
+  }
 }
