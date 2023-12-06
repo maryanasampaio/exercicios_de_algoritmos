4be21b3d37e2c003bca99513379a19ccba3a6cc5
Maryana Sampaio <maryanacem.star2@gmail.com>
2023-12-06 19:01:26 -0300

    criação do repositório local

---
+++ test/exercicios_de_algoritmos_test.dart
@@ -0,0 +1,8 @@
+import 'package:exercicios_de_algoritmos/exercicios_de_algoritmos.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
